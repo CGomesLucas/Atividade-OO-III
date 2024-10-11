19b29bb67c0b4bfc56d05a0c302c1d46a88ec082
Lucas Gomes Da Cruz <cruzlucasads1@gmail.com>
2024-10-10 22:03:12 -0300

    refrigerante.php

--- refrigerante.php
+++ refrigerante.php
@@ -3,11 +3,20 @@ require_once 'bebida.php';
 
 class Refrigerante extends Bebida {
 
+   private $refrigerante;
+
+   public function setRefrigerante($sabor){
+      $this->refrigerante = $refrigerante;
+   }
+
+   public function getRefrigerante(){
+      return $this->refrigerante;
+
    public function mostrarBebida() {
        return "Refrigerante: Marca: $this->marca, Categoria: $this->categoria, Valor: R$$this->valor, Ano: $this->ano";
    }
 
-   public function verificarPreco() {
+   public function verificarPreco($preco) {
        return $this->valor < 5;
    }
 }
