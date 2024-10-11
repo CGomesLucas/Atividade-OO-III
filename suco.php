311272ec5f72f0206be765a4b2dd5638aa7af344
Lucas Gomes Da Cruz <cruzlucasads1@gmail.com>
2024-10-10 21:58:59 -0300

    suco.php

--- suco.php
+++ suco.php
@@ -3,11 +3,21 @@ require_once 'bebida.php';
 
 class Suco extends Bebida {
 
+   private $sabor;
+
+   public function setSabor($sabor){
+      $this->sabor = $sabor;
+   }
+
+   public function getSabor(){
+      return $this->sabor;
+
+
    public function mostrarBebida() {
        return "Suco: Marca: $this->marca, Categoria: $this->categoria, Valor: R$$this->valor, Ano: $this->ano";
    }
 
-   public function verificarPreco() {
+   public function verificarPreco($preco) {
        return $this->valor < 2.5;
    }
 }
